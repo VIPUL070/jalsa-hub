446bbcf55146b64f47b11ef22f482303e982e1a8
VIPUL070 <sutharvipul007@gmail.com>
2026-04-24 15:20:55 +0530

    Creating register route for the user

--- server/src/controller/authController.ts
+++ server/src/controller/authController.ts
@@ -1,12 +1,77 @@
 import type { Request, Response } from "express";
+import zod from 'zod'
+import bcrypt from "bcryptjs";
+import jwt from 'jsonwebtoken';
+import { User } from "../models/user.js";
+import { sendOtpEmail } from "../utils/email.js";
+import { Otp } from "../models/otp.js";
 
-export const registerUser = async (req:Request,res:Response) => {
-   
+const JWT_SECRET = process.env.JWT_SECRET as string
+
+const registerSchema = zod.object({
+    name: zod.string().min(1),
+    email: zod.email(),
+    password: zod.string().min(1)
+});
+const generateOTP = () => Math.floor(100000 + Math.random() * 900000).toString();
+
+export const registerUser = async (req: Request, res: Response) => {
+    const response = registerSchema.safeParse(req.body);
+    const { name, email, password } = req.body;
+
+    if (!response.success) {
+        return res.status(411).json({
+            message: "Error in inputs"
+        })
+    }
+
+    try {
+        const userExist = await User.findOne({
+            name: name
+        })
+
+        if (userExist) {
+            return res.status(403).json({
+                messgae: "User already exist with this username"
+            })
+        }
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password,salt)
+
+        const newUser = await User.create({
+            name,
+            email,
+            password: hashedPassword
+        })
+        
+        // const otp = generateOTP();
+        // console.log(`OTP for ${email}: ${otp}`)
+        // await Otp.create({email,otp, action: 'account_verification'})
+        // await sendOtpEmail(email,otp,'account_verification');
+        
+        
+        res.status(200).json({
+            message: "User registered successfully , Please check your email to verify your account",
+            email,
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            message: (error as Error).message
+        })
+    }
 }
 
-export const loginUser = async (req:Request,res:Response) => {
+const loginSchema = zod.object({
+    email: zod.email(),
+    password: zod.string().min(1)
+});
+
+export const loginUser = async (req: Request, res: Response) => {
+    
 
 }
-export const verifyOtp = async (req:Request,res:Response) => {
+
+export const verifyOtp = async (req: Request, res: Response) => {
 
 }
---
+++ server/src/models/otp.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import { required } from "zod/mini";
+
+const otpSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    otp: {
+        type: String,
+        required: true
+    },
+    action: {
+        type: String,
+        enum : ['account_verification', 'event_booking'],
+        required: true
+    },
+    createdAt : {
+        type: Date,
+        default: Date.now,
+        expires : 300    //after 5 mins
+    }
+});
+
+export const Otp = mongoose.model('OTP', otpSchema);

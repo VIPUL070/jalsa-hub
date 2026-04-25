df2da9b99e26edc800a35506bf9ba4c6d8dbcad7
VIPUL070 <sutharvipul007@gmail.com>
2026-04-25 14:24:31 +0530

    Define all the routes for the events

--- server/src/routes/event.ts
+++ server/src/routes/event.ts
@@ -1,6 +1,23 @@
 import { Router } from "express";
+import { authMiddleware } from "../middleware/authMiddleware.js";
+import { adminMiddleware } from "../middleware/adminMiddleware.js";
 
 const router = Router();
-router.post('/', )
+
+//get event for all users
+router.get('/', getAllEvents);
+
+//get event for particular user based on id
+router.get('/:id', authMiddleware, getEventById);
+
+//create an event (for admin only)
+router.post('/' , authMiddleware , adminMiddleware , createEvent);
+
+//update an event (for admin only)
+router.put('/:id' ,authMiddleware, adminMiddleware, updateEvent);
+
+//delete an event (for admin only)
+router.delete('/:id', authMiddleware, adminMiddleware, deleteEvent);
+
 
 export default router;

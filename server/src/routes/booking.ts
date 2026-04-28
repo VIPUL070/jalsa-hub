5d9ad5140fb583962899f698a59c89943dea6124
VIPUL070 <sutharvipul007@gmail.com>
2026-04-28 19:53:29 +0530

    Writing routes for booking route

--- server/src/routes/booking.ts
+++ server/src/routes/booking.ts
@@ -1,5 +1,14 @@
 import { Router } from "express";
+import { authMiddleware } from "../middleware/authMiddleware.js";
+import { adminMiddleware } from "../middleware/adminMiddleware.js";
+import { bookEvent, cancelBooking, confirmBooking, getMyBookings, sendBookingOTP } from "../controller/bookingController.js";
 
 const router = Router();
+
+router.post('/', authMiddleware , bookEvent);
+router.post('send-otp', authMiddleware, sendBookingOTP);
+router.get('/my', authMiddleware , getMyBookings);
+router.patch('/:id/confirm', authMiddleware, adminMiddleware, confirmBooking);
+router.delete('/:id', authMiddleware, cancelBooking)
 
 export default router;

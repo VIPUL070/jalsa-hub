4dd32ea854b063e8ee555e4ec7ea4829649914a1
VIPUL070 <sutharvipul007@gmail.com>
2026-04-29 14:03:01 +0530

    Updating booking model

--- server/src/models/bookings.ts
+++ server/src/models/bookings.ts
@@ -3,20 +3,31 @@ import mongoose from "mongoose"
 const bookingSchema = new mongoose.Schema({
     userId : {
         type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    eventId : {
+        type: mongoose.Schema.Types.ObjectId,
         ref: 'Event',
         required: true
     },
     status: {
-        type: ['pending', 'confirmed', 'cancelled'],
+        type: String,
+        enum: ['pending', 'confirmed', 'cancelled'],
         default: 'pending'
     },
     paymentStatus: {
-        tyep: ['non_paid','paid'],
+        type: String,
+        enum: ['non_paid','paid'],
         default: 'non_paid'
     },
     amount: {
         type: Number,
         required: true
+    },
+    booked_at: {
+        type: Date,
+        default: Date.now
     }
 }, {timestamps: true})
 

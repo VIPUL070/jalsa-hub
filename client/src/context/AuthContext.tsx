02138564c08bb960e03aaa1513b0f82ad56ecb1d
VIPUL070 <sutharvipul007@gmail.com>
2026-05-02 22:49:11 +0530

    Writing basic authContext of the app (not finished)

---
+++ client/src/context/AuthContext.tsx
@@ -0,0 +1,56 @@
+import { createContext, useEffect, useState } from "react";
+import type { ReactNode } from "react";
+
+interface User{
+  name?: string,
+  email?: string,
+  password?: string,
+  otp?: unknown
+}
+
+interface AuthContextType {
+  user: User | null;
+  loading: boolean;
+  login: (userData: User) => void;
+  logout: () => void;
+}
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+export const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider = ({ children }: AuthProviderProps) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      try {
+        const parsedUser = JSON.parse(storedUser);
+        setUser(parsedUser);
+      } catch (error) {
+        console.error("Error parsing user", error);
+      }
+    }
+    setLoading(false);
+  }, []);
+
+  const login = (userData: User) => {
+    setUser(userData);
+    localStorage.setItem("user", JSON.stringify(userData));
+  };
+
+  const logout = () => {
+    setUser(null);
+    localStorage.removeItem("user");
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, loading, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};

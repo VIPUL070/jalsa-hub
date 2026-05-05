ee48ce8670a86d815f770f34864883c0ba9fe6e9
VIPUL070 <sutharvipul007@gmail.com>
2026-05-05 16:04:54 +0530

    Creating signin page for the app

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import React, { useContext, useState } from "react";
 import { AuthContext, type AuthContextType } from "../context/AuthContext";
 import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
